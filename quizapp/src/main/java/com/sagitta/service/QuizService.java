31646d567024985194f9f256a71563770b402895
visha <visha@DESKTOP-9KM9NCD>
2023-08-15 21:06:57 +0530

    Eight Commit
    Completion of Monolithic Quiz Application

--- quizapp/src/main/java/com/sagitta/service/QuizService.java
+++ quizapp/src/main/java/com/sagitta/service/QuizService.java
@@ -1,6 +1,8 @@
 package com.sagitta.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,8 +11,10 @@ import org.springframework.stereotype.Service;
 
 import com.sagitta.dao.QuestionDAO;
 import com.sagitta.dao.QuizDAO;
+import com.sagitta.domain.QuestionWrapper;
 import com.sagitta.domain.Questions;
 import com.sagitta.domain.Quiz;
+import com.sagitta.domain.Response;
 
 @Service
 public class QuizService {
@@ -20,14 +24,42 @@ public class QuizService {
 
 	@Autowired
 	QuestionDAO questionDAO;
+
 	public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
-		List<Questions> questionList =  questionDAO.findRandomQuestionByCategory(category, numQ);
-		
+		List<Questions> questionList = questionDAO.findRandomQuestionByCategory(category, numQ);
+
 		Quiz quiz = new Quiz();
 		quiz.setTitle(title);
 		quiz.setQuestionList(questionList);
 		quizDAO.save(quiz);
-		return new ResponseEntity<>("Success",HttpStatus.CREATED);
+		return new ResponseEntity<>("Success", HttpStatus.CREATED);
 	}
-	
+
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+		Optional<Quiz> quiz = quizDAO.findById(id);
+		List<Questions> queFromDB = quiz.get().getQuestionList();
+		List<QuestionWrapper> queFromClient = new ArrayList<QuestionWrapper>();
+
+		queFromDB.forEach(que -> {
+			QuestionWrapper qw = new QuestionWrapper(que.getId(), que.getQuestionTitle(), que.getOption1(),
+					que.getOption2(), que.getOption3(), que.getOption4());
+			queFromClient.add(qw);
+		});
+		return new ResponseEntity<List<QuestionWrapper>>(queFromClient, HttpStatus.OK);
+	}
+
+	public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+		Quiz quiz = quizDAO.findById(id).get();
+		List<Questions> queList = quiz.getQuestionList();
+		int right = 0;
+		Integer index = 0;
+		for (Response response : responses) {
+			if (response.getRight_answer().equals(queList.get(index).getRightAnswer())) {
+				right++;
+			}
+			index++;
+		}
+		return new ResponseEntity<Integer>(right, HttpStatus.OK);
+	}
+
 }
